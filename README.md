2565e22d31f7ca6c6239fa415626935156da77bb
Luca Palma <lucapalma2002@gmail.com>
2024-11-13 14:50:40 +0100

    Add initial Danea Easyfatt integration functionality

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # danea-easyfatt-integration
+
 Laravel package for integrating Danea Easyfatt.
---
+++ src/DaneaEasyfattServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace YourNamespace\DaneaEasyfatt;
+
+use Illuminate\Support\ServiceProvider;
+
+class DaneaEasyfattServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register any bindings or configurations here
+    }
+
+    public function boot()
+    {
+        // Boot any package services here
+    }
+}
